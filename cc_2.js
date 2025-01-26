12efe43e76881b11d4809242eaf82248eb9be8e5
iankalcevic <iankalcevic@usf.edu>
2025-01-26 16:38:58 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -4,3 +4,12 @@ products.push("Juice");
 console.log(products);
 products.pop();
 console.log("Updated products list",products);
+
+// Task 2 - Accessing and Modifying Arrays
+let scores = [10, 9, 7, 5, 3]
+scores[1] = 9;
+let total = scores.reduce((a, b ) => a + b, 0);
+let average = total / scores.length;
+console.log("Updated Scores;", scores);
+console.log(total);
+console.log("Average Score:", average);
