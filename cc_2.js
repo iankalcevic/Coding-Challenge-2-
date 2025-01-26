f057d2b3236484f0781607a48382438a4918d46d
iankalcevic <iankalcevic@usf.edu>
2025-01-26 16:38:14 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,6 @@
+// Task 1 - Working with arrays
+let products = ["Milk", "Bread", "Cheese", "Water Bottles", "Fruit"]; // Array of products
+products.push("Juice"); 
+console.log(products);
+products.pop();
+console.log("Updated products list",products);
